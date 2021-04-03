2342da809f99d0295e46d84623b04f672bf4e6b7
Nazeel <nazeelash@gmail.com>
2021-04-04 00:47:52 +0530

    added console interface - cli

--- src/main/java/driver/DriverApp.java
+++ src/main/java/driver/DriverApp.java
@@ -5,32 +5,89 @@ import gameoflife.GameOfLifeSolver;
 import salestax.SalesTaxSolver;
 import util.ETestCaseFile;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class DriverApp {
 
     public static void main(String[] args) throws Exception {
 
-        DriverApp driver = new DriverApp();
 
-//        driver.solveSalesTaxProblem();
-        driver.solveGameOfLife();
+            DriverApp driver = new DriverApp();
+
+            if(args.length==0) {
+                printMenu(true);
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+                String choice;
+                    while((choice = reader.readLine()) != null && !choice.equals("3")) {
+                        try {
+                            if("1".equals(choice)) {
+                                System.out.println("Sales Tax calculator\nEnter input test file: ");
+                                String fileName = reader.readLine();
+                                System.out.println("Output: ");
+                                driver.solveSalesTaxProblem(fileName);
+                                printMenu(true);
+                            } else if ("2".equals(choice)) {
+                                System.out.println("Game of Life tick generator\nEnter input test file: ");
+                                String fileName = reader.readLine();
+                                System.out.println("Output: ");
+                                driver.solveGameOfLife(fileName);
+                                printMenu(true);
+                            } else {
+                                System.out.println("Invalid choice: '"+choice+"'. Please try again.");
+                                printMenu(false);
+                            }
+                        } catch (Exception e) {
+                            System.out.println("An error has occurred. Going back to menu...");
+                            printMenu(true);
+                        }
+                    }
+
+            } else {
+                try {
+                    if(args[0].equals("1")) {
+                        System.out.println("Output: ");
+                        driver.solveSalesTaxProblem(args[1]);
+                    } else if(args[0].equals("2")) {
+                        System.out.println("Output: ");
+                        driver.solveGameOfLife(args[1]);
+                    } else {
+                        System.out.println("Invalid arg: '"+args[0]+"'. Please try again.");
+                    }
+                } catch (Exception e) {
+                    System.out.println("An error has occurred.");
+                }
+            }
+
+
     }
 
-    private void solveSalesTaxProblem() throws Exception {
+    private static void printMenu(Boolean printIntro){
+        if(printIntro) System.out.println("\nHello and welcome to the Sales Tax calculator and Game of Life tick generator.");
+        System.out.println("Select the program you would like to run:\n\t1. Sales Tax\n\t2. Game of Life\n\t3. Exit");
+        System.out.print("Enter your choice: ");
+    }
+
+    private void solveSalesTaxProblem(String fileName) throws Exception {
         SalesTaxSolver solver = new SalesTaxSolver();
 
-        solver.solve(ETestCaseFile.salesTaxInput1);
-        System.out.println("---------------");
-        solver.solve(ETestCaseFile.salesTaxInput2);
-        System.out.println("---------------");
-        solver.solve(ETestCaseFile.salesTaxInput3);
+//        solver.solve(ETestCaseFile.salesTaxInput1);
+//        System.out.println("---------------");
+//        solver.solve(ETestCaseFile.salesTaxInput2);
+//        System.out.println("---------------");
+//        solver.solve(ETestCaseFile.salesTaxInput3);
+
+        solver.solve(fileName);
     }
 
-    private void solveGameOfLife() throws Exception {
+    private void solveGameOfLife(String fileName) throws Exception {
         GameOfLifeSolver solver = new GameOfLifeSolver();
 //        solver.solve(ETestCaseFile.gameOfLifeInput1);
 //        System.out.println("---------------");
 //        solver.solve(ETestCaseFile.gameOfLifeInput2);
-        solver.solve(ETestCaseFile.gameOfLifeInput4);
+        solver.solve(fileName);
     }
 
 }
--- src/main/java/util/TestCaseLoader.java
+++ src/main/java/util/TestCaseLoader.java
@@ -4,19 +4,30 @@ import java.io.*;
 
 public class TestCaseLoader {
 
-
-
-    public static final BufferedReader getInputBufferForFile(String fileName) {
+    public static final BufferedReader getInputBufferForFile(String fileName) throws FileNotFoundException {
         BufferedReader bufferedReader = null;
+        File file = new File(fileName);
 
         try {
-            bufferedReader = new BufferedReader(new InputStreamReader(
-                    TestCaseLoader.class.getClassLoader().getResourceAsStream(fileName))
-            );
-
-        } catch (SecurityException e) {
-            e.printStackTrace();
+            bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
         }
+        catch (FileNotFoundException e) {
+
+            try {
+                bufferedReader = new BufferedReader(new InputStreamReader(
+                        TestCaseLoader.class.getClassLoader().getResourceAsStream(fileName))
+                );
+
+                if(bufferedReader == null) {
+                    System.out.println("The file '"+fileName+"' could not be found.");
+                    throw e;
+                }
+            } catch (SecurityException ex) {
+                System.out.println("The file '"+fileName+"' could not be found.");
+                throw ex;
+            }
+        }
+
 
         return bufferedReader;
     }
